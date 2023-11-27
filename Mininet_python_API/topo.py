ee4b306375a170f8f0a4754d6213ec469b73e8a7
ntu002 <ngoctubn2002@gmai.com>
2023-11-28 03:14:43 +0700

    Test: python API

---
+++ Mininet_python_API/topo.py
@@ -0,0 +1,22 @@
+from mininet.net import Mininet
+from mininet.topo import Topo
+
+net = Mininet()
+
+# Creating nodes in the network
+c0 = net.addController()
+h0 = net.addHost('h0')
+s0 = net.addSwitch('s0')
+h1 = net.addHost('h1')
+
+#Createing links between nodes in network (2-ways)
+net.addLink(h0, s0)
+net.addLink(h1, s0)
+
+#Configuration of IP addresses in interfaces
+h0.setIP('192.168.1.1', 24)
+h1.setIP('192.168.1.2', 24)
+
+net.start()
+net.pingAll()
+net.stop()
